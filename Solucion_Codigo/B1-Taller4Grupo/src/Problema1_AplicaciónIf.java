c7958f440e35c4869f6a0f4b40fcab75b660591b
MRodz <mabarca1@utpl.edu.ec>
2024-05-07 12:33:22 -0500

    Ejerciazo 1

--- Solucion_Codigo/B1-Taller4Grupo/src/Problema1_AplicaciónIf.java
+++ Solucion_Codigo/B1-Taller4Grupo/src/Problema1_AplicaciónIf.java
@@ -1,6 +1,58 @@
 
 public class Problema1_AplicaciónIf {
+
+
+
+    private String name;
+    private double price;
+    private int quant;
+    private double desc;
+    private double finalPrice;
+
+    public Problema1_AplicaciónIf(String name, double price, int quant) {
+        this.name = name;
+        this.price = price;
+        this.quant = quant;
+    }
+
+    public void calcularDescuento(double price, int quant) {
+        double newPrice;
+        if (price >= 1000 && quant >= 10) {
+
+            this.desc = price * (0.1);
+
+        } else if (price < 1000) {
+
+            this.desc = price * (0.05);
+        } else {
+            this.desc = 0;
+        }
+
+    }
+
+    public double getDiscount() {
+        return this.desc;
+    }
+
+    public void calcularPrecioFinal(double price, double desc) {
+        this.finalPrice = price-desc;
+        
+    }
     
-    private double precio;
-    private double quant;
+    public double getPrecioFinal() {
+        return this.finalPrice;
+    }
+    
+        @Override
+    public String toString() { // String BUilderaso
+        StringBuilder sb = new StringBuilder();
+        sb.append("Productos{");
+        sb.append("Nombre=").append(name);
+        sb.append(", precio=").append(price);
+        sb.append(", cantidad=").append(quant);
+        sb.append(", descuento=").append(getDiscount());
+        sb.append(", Precio Final=").append(getPrecioFinal());
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- Solucion_Codigo/B1-Taller4Grupo/src/Problema1_AplicaciónIfEjecutor.java
+++ Solucion_Codigo/B1-Taller4Grupo/src/Problema1_AplicaciónIfEjecutor.java
@@ -1,7 +1,22 @@
 
+import java.util.Scanner;
+
 public class Problema1_AplicaciónIfEjecutor {
-    
+
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingrese nombre del producto");
+        String name = in.nextLine();
+        System.out.println("Ingrese precio del producto");
+        double price = in.nextDouble();
+        System.out.println("Ingrese cantidad del producto");
+        int quant = in.nextInt();
         
+        Problema1_AplicaciónIf productito = new Problema1_AplicaciónIf(name, price, quant);
+        
+        productito.calcularDescuento(price, quant);
+        productito.calcularPrecioFinal( price, productito.getDiscount());
+        
+        System.out.println(productito.toString());
     }
 }
